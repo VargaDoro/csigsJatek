6a4f70950a2913e366a63cca8b6baa5b3e00a20a
VargaDoro <varga.dorothea.timea@gmail.com>
2025-03-26 10:50:54 +0100

    csiga oszt

--- src/main/CsigasJatek.java
+++ src/main/CsigasJatek.java
@@ -1,9 +1,35 @@
 package main;
 
+import java.util.Random;
+
 public class CsigasJatek extends Jatek{
-    @Override
-    void start(){
-        System.out.println("Csigás indúl");
-    };
+    private static final Random RND = new Random();
+//    @Override
+//    void start(){
+//        System.out.println("Csigás indúl");
+//    };
+    private Csiga[] csigak;
     
+    public void csigakLetrehozasa(){
+        csigak[0] = new Csiga("piros");
+        csigak[1] = new Csiga("zold");
+        csigak[2] = new Csiga("kek");
+    } 
+    
+    public Csiga csigagyorsitas(){
+        int eselySzam = RND.nextInt(0, 100);
+        boolean esely = false;
+        if (eselySzam < 20) {
+            esely = true;
+            Csiga valasztott;
+            int csigaSzam = RND.nextInt(0, 3);
+            valasztott = csigak[csigaSzam];
+        }
+        
+        return ;
+    }
+    
+    public void verseny(){
+        
+    }
 }
