a9c13bc99add3a7adf70a8a417f5f614343a6ab1
VargaDoro <varga.dorothea.timea@gmail.com>
2025-03-26 09:22:54 +0100

    szia

--- src/main/Csiga.java
+++ src/main/Csiga.java
@@ -3,8 +3,11 @@ package main;
 public class Csiga {
     private String szin, megtettUt, abra, szinKod;
     private boolean gyorsito;
+    private char utjel;
     
-    public Csiga(String szin, String megtettUt, String abra, String szinKod, boolean gyorsito){
-        
+    public Csiga(String szin){
+        this.szin = szin;
     }
+    
+    
 }
