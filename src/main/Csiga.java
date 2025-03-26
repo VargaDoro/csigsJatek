e2a675b81ad64a7325c5fed28edbf9ee58750970
VargaDoro <varga.dorothea.timea@gmail.com>
2025-03-26 09:38:36 +0100

    rövidítve

--- src/main/Csiga.java
+++ src/main/Csiga.java
@@ -1,13 +1,20 @@
 package main;
 
 public class Csiga {
-    private String szin, megtettUt, abra, szinKod;
+    private String szin, megtettUt, szinKod;
     private boolean gyorsito;
-    private char utjel;
+    private static final String ABRA;
+    private static final char UTJEL_1, UTJEL_2;
     
     public Csiga(String szin){
         this.szin = szin;
     }
     
+    private void setSzinkod(String szinKod){
+        this.szinKod = szinKod;
+    } 
     
+    public void megy(){
+        
+    }
 }
